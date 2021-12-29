f3350cb9f16147fd584d4455b29bf80d402b3b74
heaphub <61900994+heaphub@users.noreply.github.com>
2021-12-29 13:08:34 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM debian:buster
 
 LABEL maintainer="imfanshilin@gmail.com"
 
-COPY install_vapoursynth_tools.sh /opt
+COPY vstool_install.sh /opt
 
 RUN apt update -y \
     && apt upgrade -y \
@@ -13,12 +13,15 @@ RUN apt update -y \
                     libboost-system-dev python3-pip \
     && pip3 install cython \
     && cd /opt \
-    && bash install_vapoursynth_tools.sh \
+    && bash vstool_install.sh \
     && localedef -c -f UTF-8 -i zh_CN zh_CN.utf8 \
     && apt autoremove -y build-essential pkg-config cmake gcc g++ nasm yasm autoconf automake libtool zlib1g-dev \
                     libboost-filesystem-dev libboost-system-dev \
-    && rm -f /opt/install_vapoursynth_tools.sh
+    && rm -f /opt/vstool_install.sh
 
-ENV LANG zh_CN.utf8
-WORKDIR /root
+ENV LANG=zh_CN.utf8 \
+    PYTHONPATH=/usr/local/lib/python3.7/site-packages \
+    LD_LIBRARY_PATH=/usr/local/lib
+
+WORKDIR /VSToolNix
 CMD ["/bin/bash"]
---
+++ vstool_install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+LD_LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH
+PYTHONPATH=/usr/local/lib/python3.7/site-packages
+export PYTHONPATH
+
+# zimg
+cd /opt
+git clone https://github.com/sekrit-twc/zimg.git
+cd zimg 
+./autogen.sh 
+./configure
+make -j$(nproc)
+make install
+cd .. && rm -rf zimg
+
+# vapoursynth R53
+cd /opt
+git clone -b R53 https://github.com/vapoursynth/vapoursynth.git 
+cd vapoursynth 
+./autogen.sh
+./configure
+make -j$(nproc)
+make install
+cd .. && rm -rf vapoursynth
+
+# vapoursynth-plugins
+cd /opt
+git clone https://github.com/heaphub/vapoursynth-plugins.git
+cd vapoursynth-plugins
+./autogen.sh
+./configure
+make -j$(nproc)
+make install
+mv scripts /opt/vapoursynth-scripts
+cd .. && rm -rf vapoursynth-plugins
+
+# vapoursynth-tonema
+cd /opt
+git clone https://github.com/ifb/vapoursynth-tonemap.git
+cd vapoursynth-tonemap
+./autogen.sh
+./configure
+make -j$(nproc)
+make install
+mv /usr/local/lib/libtonemap.so /usr/local/lib/vapoursynth
+mv /usr/local/lib/libtonemap.la /usr/local/lib/vapoursynth
+cd .. && rm -rf vapoursynth-tonemap
